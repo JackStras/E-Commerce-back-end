a2dd34041f550bda6e1122c79f9e5e61ec89bb5c
JackStras <jackstrasbaugh@yahoo.com>
2023-11-27 16:12:38 -0500

    Completed unfinished routes in tag-routes and category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,24 +4,101 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+    .then(data => {
+      if(!data) {
+        res.status(404).json({message: 'No categories'});
+        return;
+      }
+      res.json(data)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json(error)
+    })
   // find all categories
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+    .then(data => {
+      if(!data) {
+        res.status(404).json({message: 'No categories'});
+        return;
+      }
+      res.json(data)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json(error)
+    })
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(data => res.json(data))
+    .catch(error => {
+      console.log(error)
+      res.status(500).json(error)
+    })
   // create a new category
 });
 
 router.put('/:id', (req, res) => {
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(data => {
+      if (!data) {
+        res.status(404).json({message:'id does not match category'})
+        return
+      }
+      res.json(data)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json(error)
+    })
   // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(data => {
+      if (!data){
+        res.status(404).json({message: 'id does not match category'});
+        return;
+      }
+      res.json(data)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json(error)
+    })
   // delete a category by its `id` value
 });
 
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,24 +4,89 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
+  Tag.findAll({
+    include: {
+      model: Product,
+      attributes: ['product_name', 'price', 'stock', 'category_id']
+    }
+  })
+    .then(data => res.json(data))
+    .catch(error => {
+      console.log(error)
+      res.status(500).json(error)
+    })
   // find all tags
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['product_name', 'price', 'stock', 'category_id']
+    }
+  })
+    .then(data => res.json(data))
+    .catch(error => {
+      console.log(error)
+      res.status(500).json(error)
+    })
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+    .then(data => res.json(data))
+    .catch(error => {
+      console.log(error)
+      res.status(500).json(error)
+    })
   // create a new tag
 });
 
 router.put('/:id', (req, res) => {
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(data => {
+      if (!data){
+        res.status(404).json({message:'id does not match any tags'});
+        return;
+      }
+      res.json(data);
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json(error)
+    })
   // update a tag's name by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => {
+    if (!data) {
+      res.status(404).json({message: 'id does not match any tags'});
+      return;
+    }
+    res.json(data)
+  })
+  .catch(error =>{
+    console.log(error)
+    res.status(500).json(error)
+  })
   // delete on tag by its `id` value
 });
 

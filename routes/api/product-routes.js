d7e7c36af399452c3d1596b5656bd336b187718e
JackStras <jackstrasbaugh@yahoo.com>
2023-11-27 15:58:38 -0500

    Completed unfinished routes in product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,12 +5,57 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }
+    ]
+  })
+    .then(data => res.json(data))
+    .catch(error => {
+      console.log(error);
+      res.status(500).json(error);
+    })
   // find all products
   // be sure to include its associated Category and Tag data
-});
+})
 
 // get one product
 router.get('/:id', (req, res) => {
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }
+    ]
+  })
+    .then(data => {
+      if (!data) {
+        res.status(404).json({message: 'id does not match any products'});
+        return;
+      }
+      res.json(data);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json(error)
+    })
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
@@ -93,6 +138,22 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => {
+    if (!data) {
+      res.status(404).json({message: 'id does not match any products'});
+      return;
+    }
+    res.json(data);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json(error);
+  });
   // delete one product by its `id` value
 });
 
